d170b2c5f9d607efe1bb10ef7d8d7319fa426767
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-04-21 11:04:50 +0900

    おおお

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,89 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "ImGuiManager.h"
+#include"PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("detheBall.png");
+
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {400, 200});
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	/*--------音楽---------*/
+	// サウンドデータの読み込み
+	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
+	// 音声再生
+	audio_->PauseWave(soundDataHandle_);
+	//音楽再生
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
+	// デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+	
+
+
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// スプライトの今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	// 座標を{ 2, 1 }移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	//スペースを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+	//音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+	// デバックテキストの表示
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 32);
+	ImGui::End();
+
+	ImGui::Begin("Debug1");
+	// float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	// float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+
+	// デモウィンドウの表示を有効化
+	ImGui::ShowDemoWindow();
+	debugCamera_->Update();
+
+}
 
 void GameScene::Draw() {
 
@@ -27,7 +97,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -41,9 +111,20 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	// 3Dモデル描画
+	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
+	
+		// ラインを描画する
+		PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
+
+		
+
+
+	
 #pragma endregion
 
 #pragma region 前景スプライト描画
