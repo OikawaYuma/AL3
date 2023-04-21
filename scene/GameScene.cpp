cce713dd23f13819dc617a8ddc15cb2bab86e3a9
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-04-21 14:39:42 +0900

    終了！！

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include "ImGuiManager.h"
 #include"PrimitiveDrawer.h"
+#include"AxisIndicator.h"
 
 GameScene::GameScene() {}
 
@@ -45,6 +46,11 @@ void GameScene::Initialize() {
 
 	// デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
+
+	// 軸方向の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 	
 
 
