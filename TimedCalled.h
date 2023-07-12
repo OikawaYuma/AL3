6c2516eeafa843b28ce62874952b4cb5121197b0
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-07-12 14:07:45 +0900

    途中

---
+++ TimedCalled.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include<functional>
+
+/// <summary>
+/// 時限発動
+/// </summary>
+class TimedCalled {
+public:
+	// コンストラクタ
+	TimedCalled(std::function<void(int)> hr,uint32_t time);
+	// 更新
+	void Update();
+	// 完了ならtrueを返す
+	bool IsFinished() { return TimeCall = true; };
+
+private:
+	// コールバック
+	std::function<void(int)> hr_ = [](int i) { i-= 1; };
+	//残り時間
+	uint32_t time_;
+	// 完了フラグ
+	bool TimeCall = false;
+
+};
